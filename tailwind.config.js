f724d9f4fbdb0dd8c9cc87e606769fedb2bbf1d3
Catherine <catherine.ohara@scalens.com>
2024-10-06 23:10:43 +0200

    add font stylesheets, delete boilerplate css, extend fonts in tailwind theme, add logo to assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body: 'Manrope',
+        heading: 'Sora',
+        sans: ['Manrope'],
+      }
+    },
   },
   plugins: [],
 }
